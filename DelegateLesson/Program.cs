249ffe48c4d95084680564a12b907f314658ec61
Vyacheslav Kachenyuk <vkachenyuk@vkachenyuk.playtika.local>
2015-12-02 16:20:14 +0200

    updated DelegateLesson project

--- DelegateLesson/Program.cs
+++ DelegateLesson/Program.cs
@@ -2,14 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace DelegateLesson
+namespace CSharpDemo
 {
     class Program
     {
+        delegate void DemoDelegate();
+
         static void Main(string[] args)
         {
+            DemoDelegate delegateHello = new DemoDelegate(MethodHello);
+            DemoDelegate delegateHi = new DemoDelegate(MethodHi);
+            delegateHello();
+            delegateHi();
+            Console.WriteLine("--------------------");
+
+            DemoDelegate multiDelegate = new DemoDelegate(MethodHello);
+            multiDelegate += MethodHi;
+            multiDelegate();
+            Console.WriteLine("--------------------");
+
+            multiDelegate -= MethodHi;
+            multiDelegate();
+            Console.WriteLine("--------------------");
+
+            multiDelegate = MethodHi;
+            multiDelegate();
+
+            Console.ReadKey();
+        }
+
+        static void MethodHello()
+        {
+            Console.Write("--Hello!\n");
+        }
+        static void MethodHi()
+        {
+            Console.Write("--Hi!\n");
         }
     }
 }
